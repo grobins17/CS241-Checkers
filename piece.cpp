551d5a87e33c8fc784f77f2c727b8c1bde42308d
sondande <sondande@oberlin.edu>
2020-12-01 14:59:14 -0500

    Mixed up files and switched names
    Made the foundation for the square class. Need to add the method to
    check if we have any neighbors and if we can catpure a piece or not.
    Included constructor, getRow, getCol, and getType

--- piece.cpp
+++ piece.cpp
@@ -1,53 +1,7 @@
-//Square Class
-
 #include<iostream>
-#include<piece.h>
 
 using namespace std;
 
-class Square{
-	//Values unless specified automatically are turned to private
+int main(){
 	
-	//row the square is located in the 2D array
-	int row;
-	
-	//col the square is located in the 2D array
-	int col;
-	
-	//the type that is being held in square
-	//0 is represents the square is empty
-	//1 is for user piece
-	//2 is for computer's piece
-	int type;	
-	public:
-		//calls constructor for the Square object 
-		Square(int, int, int);	
-		
-		//gets row 
-		int getRow(){
-			return row;
-		}
-		
-		//gets col
-		int getCol(){
-                        return col;
-                }
-		
-		//gets type
-		int getType(){
-                        return type;
-                }
-		
-		//need to make method to check if we have open spots. Can checkthe next squares type but will different parameters
 }
-// constructor for the Square object
-Square::Square (int a, int b, int c){
-	row = a;
-	col = b;
-	type = c;
-}
-
-
-
-
-
