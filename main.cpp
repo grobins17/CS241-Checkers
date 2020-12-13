fad5de7b2b56588ee87c9060bbc46addf0ea17e6
Garrett Robins <grobins@oberlin.edu>
2020-12-12 20:02:39 -0500

    Added turn-based gameplay

--- main.cpp
+++ main.cpp
@@ -10,95 +10,45 @@
 
 using namespace std;
 
-int main(){
-  
-  //GUI
-  initscr(); //makes screen
-  raw(); //disables line buffer, AKA you don't have to press enter for getch and wgetch
-  noecho(); //disables input displaying on screen
-  start_color(); //enables colors
-  
-  //COLOR PAIRS
-  //White square
-  init_pair(1, COLOR_WHITE, COLOR_WHITE);
-  //Black square/piece
-  init_pair(2, COLOR_BLACK, COLOR_BLACK);
-  //Red piece
-  init_pair(3, COLOR_RED, COLOR_RED);
-  //Screen border
-  init_color(COLOR_MAGENTA, 500, 500, 500);
-  init_pair(4, COLOR_MAGENTA, COLOR_MAGENTA);
-  //King Piece
-  init_color(COLOR_YELLOW, 1000, 800, 100);
-  init_pair(5, COLOR_YELLOW, COLOR_YELLOW);
-  //Highlight
-  init_pair(6, COLOR_CYAN, COLOR_CYAN);
-  
-  //TITLE BOX
-  int title_h, title_w, title_y, title_x;
+void computerturn(CheckerBoard *theboard){
+  CheckerBoard board = *(theboard);
+  Move **goodmoves = new Move *[12];
+  Move **legalmoves = new Move *[100];
+  Square blackSquare;
+  int index = 0;
+  int *place = new int[12];
+  for (int i = 0; i < 64; i++){
+    blackSquare = board.getSquare(i/8, i %8);
+    if(!blackSquare.isEmpty()){
+      if(blackSquare.ptr->isBlack()){
+	legalmoves = board.getLegalMoves(i/8, i%8);
+	if(legalmoves[0] != NULL){
+	  place[index] = i;
+	  goodmoves[index++] = legalmoves[0];
+	}
+      }
+    }
+  }
+  board.move(place[0]/8, place[0]%8, goodmoves[0]);
+}
 
-  title_h = 3;
-  title_w = 10;
-  title_y = 3;
-  title_x = (COLS - title_w) / 2;
 
-  WINDOW *title = newwin(title_h, title_w, title_y, title_x);
-  box(title, 0, 0);
-  mvwprintw(title, 1, 1, "CHECKERS");
-  wrefresh(title); 
-  
-  //BOARD EDGE
-  int edge_h, edge_w, edge_y, edge_x;
-
-  edge_h = 26;
-  edge_w = 50;
-  edge_y = (LINES/6) - 1;
-  edge_x = (COLS/2) - 25;
-
-  WINDOW *edge = newwin(edge_h, edge_w, edge_y, edge_x);
-  wattron(edge, COLOR_PAIR(4));
-  box(edge, 0, 0);
-  wattroff(edge, COLOR_PAIR(4));
-  wrefresh(edge);
-
-  //INPUT BOX
-  int in_h, in_w, in_y, in_x;
-
-  in_h = 3;
-  in_w = COLS;
-  in_y = (5 * LINES) / 6;
-  in_x = 0;
-
-  WINDOW *inBox = newwin(in_h, in_w, in_y, in_x);
-  wrefresh(inBox);
-  
-  //BOARD INITIALIZATION
-  CheckerBoard board;
-  board.initBoard();
-
-  
-  board.refreshBoard();
-  //wgetch(title);
-  keypad(inBox, TRUE); //enables usage of arrow keys
-  
-  //Start square
-  Square startSquare = board.getSquare(0, 0);
+void userturn(WINDOW *inBox, CheckerBoard *theboard){
+  CheckerBoard board = *(theboard);
+  Square startSquare = board.getSquare(7, 6);
   startSquare.highlight();
 
 //TURNS
 
-  int turns = 0;
   int ch;
-
-  int current_row = 0;
-  int current_col = 0;
+  int current_row = 7;
+  int current_col = 6;
   int selected_row = 0;
   int selected_col = 0;
   int selected=0;
 
   Square current_square = startSquare;
   Move ** legalmoves = new Move *[100];
-  while(turns < 10){
   while( selected != 1 ) {  
     ch = wgetch(inBox);
     mvwprintw(inBox, 0, 0, "Key pressed: %c", ch);
@@ -265,30 +215,88 @@ int main(){
     }
   }
   board.refreshBoard();
-  turns++;
-  }
-  // TURNS
-  /*
-  while(count < 10){
-    cout << "Current row: " << endl;
-    cin >> current_row;
-    cout << "Current col: " << endl;
-    cin >> current_col;
-    if (board.isCurrentEmpty(current_row, current_col)) {
-	cout << "Please choose a square that contains a checker piece." << "\n" << "\n";
-	continue;
-    }
-    cout << "\n";
-    cout << "Proposed row: " << endl;
-    cin >> proposed_row;
-    cout << "Proposed col: " << endl;
-    cin >> proposed_col;
-    board.printBoard();
-    count++;
-  }
+}
+
+
+
+
+int main(){
   
-  */
   //GUI
-  getch(); //REFRESHES PARENT SCREEN OVER CHILDREN
+  initscr(); //makes screen
+  raw(); //disables line buffer, AKA you don't have to press enter for getch and wgetch
+  noecho(); //disables input displaying on screen
+  start_color(); //enables colors
+  
+  //COLOR PAIRS
+  //White square
+  init_pair(1, COLOR_WHITE, COLOR_WHITE);
+  //Black square/piece
+  init_pair(2, COLOR_BLACK, COLOR_BLACK);
+  //Red piece
+  init_pair(3, COLOR_RED, COLOR_RED);
+  //Screen border
+  init_color(COLOR_MAGENTA, 500, 500, 500);
+  init_pair(4, COLOR_MAGENTA, COLOR_MAGENTA);
+  //King Piece
+  init_color(COLOR_YELLOW, 1000, 800, 100);
+  init_pair(5, COLOR_YELLOW, COLOR_YELLOW);
+  //Highlight
+  init_pair(6, COLOR_CYAN, COLOR_CYAN);
+  
+  //TITLE BOX
+  int title_h, title_w, title_y, title_x;
+
+  title_h = 3;
+  title_w = 10;
+  title_y = 3;
+  title_x = (COLS - title_w) / 2;
+
+  WINDOW *title = newwin(title_h, title_w, title_y, title_x);
+  box(title, 0, 0);
+  mvwprintw(title, 1, 1, "CHECKERS");
+  wrefresh(title); 
+  
+  //BOARD EDGE
+  int edge_h, edge_w, edge_y, edge_x;
+
+  edge_h = 26;
+  edge_w = 50;
+  edge_y = (LINES/6) - 1;
+  edge_x = (COLS/2) - 25;
+
+  WINDOW *edge = newwin(edge_h, edge_w, edge_y, edge_x);
+  wattron(edge, COLOR_PAIR(4));
+  box(edge, 0, 0);
+  wattroff(edge, COLOR_PAIR(4));
+  wrefresh(edge);
+
+  //INPUT BOX
+  int in_h, in_w, in_y, in_x;
+
+  in_h = 3;
+  in_w = COLS;
+  in_y = (5 * LINES) / 6;
+  in_x = 0;
+
+  WINDOW *inBox = newwin(in_h, in_w, in_y, in_x);
+  wrefresh(inBox);
+  
+  //BOARD INITIALIZATION
+  CheckerBoard board;
+  board.initBoard();
+  board.refreshBoard();
+
+  //wgetch(title);
+  keypad(inBox, TRUE); //enables usage of arrow keys
+  int turns = 0;
+  while(turns < 100 && board.numRed > 0 && board.numBlack > 0){
+    userturn(inBox, &board);
+    computerturn(&board);
+    board.refreshBoard();
+  }
+  usleep(3000000);
+  //GUI
+  //getch(); //REFRESHES PARENT SCREEN OVER CHILDREN
   endwin(); //ends ncurses mode
 }
